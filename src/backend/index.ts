d60a3ff00e640bcce6517f06a352e56a2d811f04
Avisek Das <avisekdas555@gmail.com>
2023-12-17 00:17:30 +0530

    refactor

--- src/backend/index.ts
+++ src/backend/index.ts
@@ -1,5 +1,5 @@
 import express from 'express' 
-// import { join } from 'node:path/posix'
+import { join } from 'node:path/posix'
 import { remultExpress } from 'remult/remult-express'
 import { Task } from '../shared/Task'
 import { TasksController } from '../shared/TasksController'
@@ -7,23 +7,20 @@ import { createPostgresDataProvider } from 'remult/postgres'
 
 export const app = express()
 
-// const api = express.Router()
-// app.use(join(import.meta.env.BASE_URL, '/api'), api)
+const api = express.Router()
+app.use(join(import.meta?.env?.BASE_URL ?? '/', '/api'), api)
 
 //#region auth
 import session from 'cookie-session'
 import type { UserInfo } from 'remult'
 
-app.use(
-  '/api',
-  session({ secret: process.env['SESSION_SECRET'] || 'my secret' })
-)
+api.use(session({ secret: process.env['SESSION_SECRET'] || 'my secret' }))
 
 export const validUsers: UserInfo[] = [
   { id: '1', name: 'Jane', roles: ['admin'] },
   { id: '2', name: 'Steve' },
 ]
-app.post('/api/signIn', express.json({ type: 'text' }), (req, res) => {
+api.post('/signIn', express.json({ type: 'text' }), (req, res) => {
   const user = validUsers.find((user) => user.name === req.body.username)
   if (user) {
     req.session!['user'] = user
@@ -33,39 +30,39 @@ app.post('/api/signIn', express.json({ type: 'text' }), (req, res) => {
   }
 })
 
-app.post('/api/signOut', (req, res) => {
+api.post('/signOut', (req, res) => {
   req.session!['user'] = null
   res.json('signed out')
 })
 
-app.get('/api/currentUser', (req, res) => {
+api.get('/currentUser', (req, res) => {
   res.json(req.session!['user'])
 })
 //#endregion
 
-app.get('/hi', (_, res) => res.send('hello'))
+api.get('/hi', (_, res) => res.send('hello'))
 
 const entities = [Task]
 
 const remultApi = remultExpress({
+  rootPath: '',
   entities,
   controllers: [TasksController],
   dataProvider: createPostgresDataProvider({
     connectionString:
       process.env['DATABASE_URL'] ||
-      'postgresql://postgres:6fBC626fBGc13E2d*F6Bd6DAGbCADfFA@viaduct.proxy.rlwy.net:29360/railway' ||
-      'postgresql://postgres:c5-Gd65CGBbdC4b6db25dbeA24Fg1Fed@viaduct.proxy.rlwy.net:57932/railway' ||
+      'postgresql://postgres:DA-1-bcbGGfC32DF3bCb*3G5C-Aa2cB4@roundhouse.proxy.rlwy.net:49408/railway' ||
       'postgres://postgres:MASTERKEY@localhost/postgres',
   }),
   getUser: (req) => req.session?.['user'],
 })
-app.use(remultApi)
+api.use(remultApi)
 
 import swaggerUi from 'swagger-ui-express'
 
 const openApiDocument = remultApi.openApiDoc({ title: 'remult-react-todo' })
-app.get('/api/openApi.json', (_, res) => res.json(openApiDocument))
-app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(openApiDocument))
+api.get('/openApi.json', (_, res) => res.json(openApiDocument))
+api.use('/docs', swaggerUi.serve, swaggerUi.setup(openApiDocument))
 
 import { remultGraphql } from 'remult/graphql'
 import { createYoga, createSchema } from 'graphql-yoga'
@@ -74,13 +71,13 @@ const { typeDefs, resolvers } = remultGraphql({
   entities: [Task],
 })
 const yoga = createYoga({
-  graphqlEndpoint: '/api/graphql',
+  graphqlEndpoint: '/graphql',
   schema: createSchema({
     typeDefs,
     resolvers,
   }),
 })
-app.use(yoga.graphqlEndpoint, remultApi.withRemult, yoga)
+api.use(yoga.graphqlEndpoint, remultApi.withRemult, yoga)
 
 if (!process.env['VITE']) {
   const frontendFiles = process.cwd() + '/dist'
@@ -88,5 +85,5 @@ if (!process.env['VITE']) {
   app.get('/*', (_, res) => {
     res.sendFile(frontendFiles + '/index.html')
   })
-  app.listen(process.env['PORT'] || 3002, () => console.log('Server started'))
+  app.listen(process.env['PORT'] ?? 3002, () => console.log('Server started'))
 }
--- vite-plugin-express.ts
+++ vite-plugin-express.ts
@@ -1,16 +1,33 @@
-export default function express(path: string) {
+import { createServer } from 'vite'
+import type { Connect, PluginOption, ViteDevServer } from 'vite'
+
+function expressMiddleware(
+  server: ViteDevServer,
+  path: string,
+): Connect.NextHandleFunction {
+  return async function expressMiddleware(req, res, next) {
+    process.env['VITE'] = 'true'
+    try {
+      const { app } = await server.ssrLoadModule(path)
+      app(req, res, next)
+    } catch (err) {
+      console.error(err)
+    }
+  }
+}
+
+export default function express(path: string): PluginOption {
   return {
-    name: "vite3-plugin-express",
-    configureServer: async (server: any) => {
-      server.middlewares.use(async (req: any, res: any, next: any) => {
-        process.env["VITE"] = "true"
-        try {
-          const { app } = await server.ssrLoadModule(path)
-          app(req, res, next)
-        } catch (err) {
-          console.error(err)
-        }
+    name: 'vite3-plugin-express',
+    configureServer: async (server) => {
+      server.middlewares.use(expressMiddleware(server, path))
+    },
+    configurePreviewServer: async (server) => {
+      const devServer = await createServer({
+        server: { middlewareMode: true },
+        appType: 'custom',
       })
+      server.middlewares.use(expressMiddleware(devServer, path))
     },
   }
 }
