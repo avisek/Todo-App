e65b1e243e312a2e2c14ed6dddd07a9cd65e22a1
Avisek Das <avisekdas555@gmail.com>
2023-12-17 20:09:53 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { dirname, resolve } from 'node:path'
-import { fileURLToPath } from 'node:url'
-import express from './vite-plugin-express'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+import express from "./vite-plugin-express";
+
+console.log(process.env);
 
 const base =
-  process.env['GITHUB_ACTIONS'] === 'true'
-    ? `/${process.env['GITHUB_REPOSITORY'].split('/')[1]}/`
-    : '/'
-const port = 3000
+  process.env["GITHUB_ACTIONS"] === "true"
+    ? `/${process.env["GITHUB_REPOSITORY"].split("/")[1]}/`
+    : "/";
+const port = 3000;
 
-const rootDir = dirname(fileURLToPath(import.meta.url))
-const srcDir = resolve(rootDir, './src')
-const distDir = resolve(rootDir, './dist')
-const publicDir = resolve(rootDir, './public')
-const frontendDir = resolve(srcDir, './frontend')
-const backendDir = resolve(srcDir, './backend')
+const rootDir = dirname(fileURLToPath(import.meta.url));
+const srcDir = resolve(rootDir, "./src");
+const distDir = resolve(rootDir, "./dist");
+const publicDir = resolve(rootDir, "./public");
+const frontendDir = resolve(srcDir, "./frontend");
+const backendDir = resolve(srcDir, "./backend");
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -32,8 +34,5 @@ export default defineConfig({
     outDir: distDir,
     emptyOutDir: true,
   },
-  plugins: [
-    react(),
-    express(backendDir),
-  ],
-})
+  plugins: [react(), express(backendDir)],
+});
